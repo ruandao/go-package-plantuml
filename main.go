7dffc3538847442b601d306b489a7d4f451b138d
maobuji <16770864@qq.com>
2018-04-24 14:59:26 +0800

    添加输出路径的处理

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/maobuji/go-package-plantuml/codeanalysis"
-	log "github.com/Sirupsen/logrus"
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/jessevdk/go-flags"
+	"github.com/maobuji/go-package-plantuml/codeanalysis"
 	"os"
-	"strings"
 	"path"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -15,9 +16,9 @@ func main() {
 	log.SetLevel(log.InfoLevel)
 
 	var opts struct {
-		CodeDir string `long:"codedir" description:"要扫描的代码目录" required:"true"`
-		GopathDir string `long:"gopath" description:"GOPATH目录" required:"true"`
-		OutputFile string `long:"outputfile" description:"解析结果保存到该文件中" required:"true"`
+		CodeDir    string   `long:"codedir" description:"要扫描的代码目录" required:"true"`
+		GopathDir  string   `long:"gopath" description:"GOPATH目录" required:"true"`
+		OutputFile string   `long:"outputfile" description:"解析结果保存到该文件中" required:"true"`
 		IgnoreDirs []string `long:"ignoredir" description:"需要排除的目录,不需要扫描和解析"`
 	}
 
@@ -43,28 +44,48 @@ func main() {
 		os.Exit(1)
 	}
 
-	if ! strings.HasPrefix(opts.CodeDir, opts.GopathDir) {
+	if opts.OutputFile == "" {
+		panic("输出文件未设置，使用/tmp/puml.txt做为输出文件")
+		opts.OutputFile = "/tmp/puml.txt"
+	}
+
+	currentPath := getCurrentDirectory(opts.OutputFile)
+	createErr := os.MkdirAll(currentPath, 0777)
+	if err != nil {
+		fmt.Printf("%s", createErr)
+		panic("GOPATH目录不能为空")
+		os.Exit(1)
+	}
+
+	if !strings.HasPrefix(opts.CodeDir, opts.GopathDir) {
 		panic(fmt.Sprintf("代码目录%s,必须是GOPATH目录%s的子目录", opts.CodeDir, opts.GopathDir))
 		os.Exit(1)
 	}
 
 	for _, dir := range opts.IgnoreDirs {
-		if ! strings.HasPrefix(dir, opts.CodeDir){
+		if !strings.HasPrefix(dir, opts.CodeDir) {
 			panic(fmt.Sprintf("需要排除的目录%s,必须是代码目录%s的子目录", dir, opts.CodeDir))
 			os.Exit(1)
 		}
 	}
 
 	config := codeanalysis.Config{
-		CodeDir: opts.CodeDir,
-		GopathDir :opts.GopathDir,
-		VendorDir : path.Join(opts.CodeDir, "vendor"),
-		IgnoreDirs:opts.IgnoreDirs,
+		CodeDir:    opts.CodeDir,
+		GopathDir:  opts.GopathDir,
+		VendorDir:  path.Join(opts.CodeDir, "vendor"),
+		IgnoreDirs: opts.IgnoreDirs,
 	}
 
 	result := codeanalysis.AnalysisCode(config)
 
-	result.OutputToFile("/tmp/uml.txt")
+	result.OutputToFile(opts.OutputFile)
 
 }
 
+func getCurrentDirectory(tempFile string) string {
+	dir, err := filepath.Abs(filepath.Dir(tempFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Replace(dir, "\\", "/", -1)
+}
