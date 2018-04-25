681198a8a3607f830f849314cd91d8fda8561f89
maobuji <16770864@qq.com>
2018-04-25 16:32:10 +0800

    添加tidb的分析工具

--- goplantuml/tidbuml/tidb-uml.sh
+++ goplantuml/tidbuml/tidb-uml.sh
@@ -2,19 +2,35 @@
 
 base_dir=`pwd`;
 tidb_src_path="/opt/gopath/src/github.com/pingcap/tidb"
+rm *.svg *.txt -rf
 
 cd ..
 
-packagelist=("ast" "cmd" "config")
+packagelist=("ast" "cmd" "config" "ddl" "distsql" "domain" "executor" "expression" "infoschema" "kv" "meta" "metrics" "model" "mysql" "owner" "parser" "perfschema" "plan" "privilege" "server" "session" "sessionctx" "statistics" "store" "structure" "table" "tablecodec" "terror" "tidb-server" "types" "util" "x-server")
+
 for package_name in ${packagelist[@]}; do
     out_file_name=$package_name.txt
-    ./go-package-plantuml --codedir $base_dir/$package_name --outputfile $out_file_name
-    echo "java -jar plantuml.jar $out_file -tsvg"
-    java -jar plantuml.jar $out_file -tsvg
+    ./go-package-plantuml --codedir $tidb_src_path/$package_name --outputfile $out_file_name
+    echo "java -jar plantuml.jar $out_file_name -tsvg"
+    java -jar plantuml.jar $out_file_name -tsvg
+    mv $package_name.txt $base_dir -f
+    mv $package_name.svg $base_dir -f
 done
 
-mv *.svg $base_dir
-rm *.txt -rf
+cd $base_dir
+
+echo "==========================uml-end============================================"
+
+for package_name in ${packagelist[@]}; do
+    if [ `grep -c "An error has occured" $base_dir/$package_name.svg` -eq '0' ]; then
+         a=""
+    else
+         echo "$package_name package error"
+    fi
+done
+
+
+
 
 
 
